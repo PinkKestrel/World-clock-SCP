3466b7a76d9937d34fdb13f27ec4c27d9de729a2
pchcarmen <125764409+PinkKestrel@users.noreply.github.com>
2025-09-17 19:48:57 +0200

    First implementation
    Added interactivity

--- javascript/index.js
+++ javascript/index.js
@@ -1,18 +1,41 @@
-function updateCities() {
-  const dateCityOne = document.querySelector("#date-city-one");
-  const timeCityOne = document.querySelector("#time-city-one");
-  dateCityOne.innerHTML = moment().tz("Europe/Madrid").format("MMMM Do YYYY");
-  timeCityOne.innerHTML = moment()
-    .tz("Europe/Madrid")
-    .format("h:mm:ss [<small>]a[</small>]");
+// Default selection
+let selectedCityTimezone = "Europe/London";
 
-  const dateCityTwo = document.querySelector("#date-city-two");
-  const timeCityTwo = document.querySelector("#time-city-two");
-  dateCityTwo.innerHTML = moment().tz("America/Lima").format("MMMM Do YYYY");
+function updateTimezones() {
+  updateLocalTimezone();
+  updateSelectedCityTimezone();
+}
+
+function updateLocalTimezone() {
+  const guestLocalZone = moment.tz.guess();
+  const cityOne = document.getElementById("city-one");
+  const dateCityOne = document.getElementById("date-city-one");
+  const timeCityOne = document.getElementById("time-city-one");
+
+  cityOne.innerHTML = guestLocalZone + " tz";
+  dateCityOne.innerHTML = moment().format("MMMM Do YYYY");
+  timeCityOne.innerHTML = moment().format("h:mm:ss [<small>]a[</small>]");
+}
+
+function updateSelectedCityTimezone() {
+  const cityTwo = document.getElementById("city-two");
+  const dateCityTwo = document.getElementById("date-city-two");
+  const timeCityTwo = document.getElementById("time-city-two");
+  cityTwo.innerHTML = selectedCityTimezone + " tz";
+  dateCityTwo.innerHTML = moment()
+    .tz(selectedCityTimezone)
+    .format("MMMM Do YYYY");
   timeCityTwo.innerHTML = moment()
-    .tz("America/Lima")
+    .tz(selectedCityTimezone)
     .format("h:mm:ss [<small>]a[</small>]");
 }
 
-updateCities();
-setInterval(updateCities, 100);
+function onSelectedCityChange(event) {
+  selectedCityTimezone = event.target.value;
+  updateSelectedCityTimezone();
+}
+
+setInterval(updateTimezones, 1000);
+
+const selectCity = document.getElementById("selectCity");
+selectCity.addEventListener("change", onSelectedCityChange);
